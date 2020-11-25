7433eef388f27a578adf3eab046935eb01ad8338
Maxim-nwb <maksimdikov@yandex.ru>
2020-11-25 17:43:17 +0300

    Minor save changes

--- Save_Statistics.py
+++ Save_Statistics.py
@@ -7,10 +7,7 @@ def save(stat):
     for i in stat:
         stat_list.append(i.split("="))
     # getting the save path
-    filepath = filedialog.asksaveasfilename(initialdir = "/",title = "Save file",filetypes=[('CSV files', '*.csv')])
-    # check .csv.csv
-    if not filepath.endswith(".csv"):
-        filepath +=".csv"
+    filepath = filedialog.asksaveasfilename(initialdir = "/",title = "Save file",filetypes=(('CSV files', '*.csv'),("jpeg files","*.jpg")), defaultextension = True)
     # save
     with open(filepath, "w", newline='') as csv_file:
         writer = csv.writer(csv_file, delimiter=':')
