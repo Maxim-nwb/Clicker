ab2668f5ce6d2bb3721bfa28f9d1b903785c5ab2
Maxim-nwb <maksimdikov@yandex.ru>
2020-11-27 13:53:02 +0300

    Added support for saving to txt

--- Save_Statistics.py
+++ Save_Statistics.py
@@ -2,14 +2,20 @@ import csv
 from tkinter import filedialog
 
 def save(stat):
-    # processing data for normal saving
-    stat_list = []
-    for i in stat:
-        stat_list.append(i.split("="))
     # getting the save path
-    filepath = filedialog.asksaveasfilename(initialdir = "/",title = "Save file",filetypes=(('CSV files', '*.csv'),("jpeg files","*.jpg")), defaultextension = True)
-    # save
-    with open(filepath, "w", newline='') as csv_file:
-        writer = csv.writer(csv_file, delimiter=':')
+    filepath = filedialog.asksaveasfilename(initialdir = "/",title = "Save file",filetypes=(('CSV files', '*.csv'),("TXT files","*.txt")), defaultextension = True)
+    # save to .csv
+    if filepath.endswith(".csv"):
+        # processing data for normal saving
+        stat_list = []
+        for i in stat:
+            stat_list.append(i.split("="))
+        with open(filepath, "w", newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=':')
         for line in stat_list:
             writer.writerow(line)
+    # save to .txt
+    if filepath.endswith(".txt"):
+        with open(filepath, "w", newline='') as txt_file:
+            for i in stat:
+                txt_file.write(i+'\n')
