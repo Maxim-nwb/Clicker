83ba8bca469659b59c306cd8e4c147598c7c7b38
Maxim-nwb <maksimdikov@yandex.ru>
2020-11-27 14:20:20 +0300

    Minor edits

--- Save_Statistics.py
+++ Save_Statistics.py
@@ -3,19 +3,20 @@ from tkinter import filedialog
 
 def save(stat):
     # getting the save path
-    filepath = filedialog.asksaveasfilename(initialdir = "/",title = "Save file",filetypes=(('CSV files', '*.csv'),("TXT files","*.txt")), defaultextension = True)
+    filepath = filedialog.asksaveasfilename(initialdir = "/",title = "Save file",filetypes=(('CSV files', '*.csv'),("TXT files","*.txt"),("All files","*.*")), defaultextension = ".txt")
     # save to .csv
     if filepath.endswith(".csv"):
         # processing data for normal saving
         stat_list = []
         for i in stat:
             stat_list.append(i.split("="))
+
         with open(filepath, "w", newline='') as csv_file:
             writer = csv.writer(csv_file, delimiter=':')
-        for line in stat_list:
-            writer.writerow(line)
+            for line in stat_list:
+                writer.writerow(line)
     # save to .txt
-    if filepath.endswith(".txt"):
+    elif filepath.endswith(".txt"):
         with open(filepath, "w", newline='') as txt_file:
             for i in stat:
                 txt_file.write(i+'\n')
