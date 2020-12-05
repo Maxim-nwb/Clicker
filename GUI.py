b85cf371f7fc05c616a972aef46f948b17ef9362
Maxim-nwb <maksimdikov@yandex.ru>
2020-12-05 18:23:47 +0300

    Small gui changes

--- GUI.py
+++ GUI.py
@@ -7,36 +7,37 @@ class MainWindow(Frame):
     def __init__(self, parent):
         Frame.__init__(self, parent)   
         self.parent = parent
-        self.parent.title("Cliker")
-        self.parent.geometry("210x150+300+300")
+        self.parent["bg"] = "#87CEEB"
+        self.parent.title("Cliker") 
+        self.parent.geometry("225x150+300+300")
         self.parent.resizable(width=False, height=False)
         self.parent.iconbitmap('tap.ico')
         self.create_widgets()
 
     def create_widgets(self):
-        self.title = Label(self.parent, text = "Actions", font="Arial 14")
-        self.title.grid(row = 0, column = 0, columnspan = 2)
+        self.title = Label(self.parent, text = "Actions", font="Arial 16", bg = "#87CEEB")
+        self.title.grid(row = 0, column = 0, columnspan = 4)
         # element that displays the count of clicks
-        self.count_cliks = Label(self.parent, text = 0)
-        self.count_cliks.grid(row = 1, column = 1)
-        self.label_cliks = Label(self.parent, text = "Cliks: ")
+        self.count_cliks = Label(self.parent, text = 0, bg = "#87CEEB", width = 3)
+        self.count_cliks.grid(row = 1, column = 1, sticky = W)
+        self.label_cliks = Label(self.parent, text = "Cliks: ", font="Arial 12", bg = "#87CEEB")
         self.label_cliks.grid(row = 1, column = 0, sticky = W)
 
         # element that displays the count of keystrokes
-        self.count_keystrokes = Label(self.parent, text = 0)
-        self.count_keystrokes.grid(row = 2, column = 1)
-        self.label_keystrokes = Label(self.parent, text = "Keystrokes: ")
+        self.count_keystrokes = Label(self.parent, text = 0, bg = "#87CEEB", width = 3)
+        self.count_keystrokes.grid(row = 2, column = 1, sticky = W)
+        self.label_keystrokes = Label(self.parent, text = "Keystrokes: ", font="Arial 12", bg = "#87CEEB")
         self.label_keystrokes.grid(row = 2, column = 0, sticky = W)
         # display button detailed statistics
-        self.stat_button = Button(self.parent, text = "Detailed statistics", command = self.onInfo)
+        self.stat_button = Button(self.parent, text = "Detailed statistics", command = self.onInfo, bg = "#E0FFFF")
         self.stat_button. grid(row = 3, column = 0, sticky = W)
         # attribute for saving statistics
         self.statistic = dict()
         # button for saving statistics
-        self.save_stat_button = Button(self.parent, text = "Save statistics", command = self.save_file)
+        self.save_stat_button = Button(self.parent, text = "Save statistics", command = self.save_file, bg = "#E0FFFF")
         self.save_stat_button. grid(row = 4, column = 0, sticky = W)
         # button for mininmize to tray
-        self.save_stat_button = Button(self.parent, text = "Minimize to tray", command = self.tray)
+        self.save_stat_button = Button(self.parent, text = "Minimize to tray", command = self.tray, bg = "#E0FFFF")
         self.save_stat_button. grid(row = 3, column = 2, sticky = W)
     # creating a report
     def create_report(self):
